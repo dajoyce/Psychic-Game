b260b4cafbe8a27429b9bfafed5f6c54285a1d28
Dee Jay <42320330+dajoyce@users.noreply.github.com>
2018-11-06 17:41:09 -0500

    Turn in Copy

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var options = ["a", "z",];
+var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 
 var wins = 0;
 var losses = 0;
@@ -12,7 +12,8 @@ var guessesSoFar = []
         console.log(userGuess);
 
         // trying to write guessesSoFar here
-        var GuessesSoFar = userGuess;
+        guessesSoFar.push(userGuess);
+        console.log(guessesSoFar); 
 
         var computerGuess = options[Math.floor(Math.random()*options.length)];
 
@@ -21,19 +22,29 @@ var guessesSoFar = []
 
         if (userGuess==computerGuess) {
             wins++
+            numGuesses = 9;
+            guessesSoFar = [];
             alert("You Guessed Correct " + wins);
-        } else {
-            losses++
-            alert("You Choose Poorly" + losses);
+        } 
+        else if (numGuesses==0){
+            losses++;
+            numGuesses = 9;
+            guessesSoFar = [];
+            alert("Try Harder - Click OK to Continue")
         }
+        else {
+            alert("You Choose Poorly");
+            numGuesses--;
+        }
+    
 
 
         var html = "<h1>The Psychic Game</h1>" +
         "<p>Guess what letter I'm thinking of</p>" +
         "<p>Wins: " + wins + "</p>" + 
         "<p>Losses: " + losses + "</p>" +
-        "<p>Guesses left:</p>" +
-        "<p>Your Guesses so far:</p>";
+        "<p>Guesses left: " + numGuesses + "</p>" +
+        "<p>Your Guesses so far: " + guessesSoFar + "</p>";
 
         document.querySelector("#game").innerHTML = html;
     }
