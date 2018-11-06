8006a8944e5f887ffff3c86379224abb4b64cb2a
Dee Jay <42320330+dajoyce@users.noreply.github.com>
2018-11-05 21:02:26 -0500

    a little bit further than I was but wanted another save point

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,41 @@
-var options = ["r", "p", "s"];
-        var wins = 0;
-        var losses = 0;
-        var ties = 0;
+var options = ["a", "z",];
 
-        document.onkeyup = function() {
-            var userguess = String.fromCharCode(event.keyCode).
-                toLowerCase();
+var wins = 0;
+var losses = 0;
+var numGuesses = 9;
+var guessesSoFar = []
+
+    document.onkeyup = function() {
+        var userGuess = String.fromCharCode(event.keyCode).
+            toLowerCase();
                 
-            console.log(userguess)
+        console.log(userGuess);
 
-            var computerGuess = options[Math.floor(Math.random()*options.length)];
+        // trying to write guessesSoFar here
+        var GuessesSoFar = userGuess;
 
-            console.log(computerGuess);
+        var computerGuess = options[Math.floor(Math.random()*options.length)];
 
-            if (userguess=="r" || userguess=="s" || userguess=="p") {
-                if ((userguess==computerGuess)) {
-                    ties++
-                    alert("Tie: " + ties);
-                }
-                if ((userguess=="r") && (computerGuess=="p")) {
-                    losses++
-                    alert("Losses: " + losses);
-                }
-                if ((userguess=="r") && (computerGuess=="s")) {
-                    wins++
-                    alert("Wins: " + wins);
-                }
-                if ((userguess=="p") && (computerGuess=="r")) {
-                    wins++
-                    alert("Wins: " + wins);
-                }
-                if ((userguess=="p") && (computerGuess=="s")) {
-                    losses++
-                    alert("Losses: " + losses);
-                }
-                if ((userguess=="s") && (computerGuess=="r")) {
-                    losses++
-                    alert("Losses: " + losses);
-                }
-                    if ((userguess=="s") && (computerGuess=="p")) {
-                    wins++
-                    alert("Wins: " + wins);
-                }
-            
-            } else {
-                alert("Please choose r, p, or s");
-            }
+        console.log(computerGuess);
+        
 
-            var html = "<h1>The Psychic Game</h1>" +
-            "<p>Guess what letter I'm thinking of</p>" +
-            "<p>Wins: " + wins + "</p>" + 
-            "<p>Losses: " + losses + "</p>" +
-            "<p>Guesses left: " + ties + "</p>" +
-            "<p>Your Guesses so far:</p>"
-
-            document.querySelector("#game").innerHTML = html;
+        if (userGuess==computerGuess) {
+            wins++
+            alert("You Guessed Correct " + wins);
+        } else {
+            losses++
+            alert("You Choose Poorly" + losses);
         }
+
+
+        var html = "<h1>The Psychic Game</h1>" +
+        "<p>Guess what letter I'm thinking of</p>" +
+        "<p>Wins: " + wins + "</p>" + 
+        "<p>Losses: " + losses + "</p>" +
+        "<p>Guesses left:</p>" +
+        "<p>Your Guesses so far:</p>";
+
+        document.querySelector("#game").innerHTML = html;
+    }
+        
+
